d4624cfd66efc65da0fb128988c6f10b67a53921
minjo <minjoon0524@gmail.com>
2023-05-21 00:28:42 +0900

    무작정 게시판 따라하기

--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -3,9 +3,15 @@ package com.study.board.service;
 import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 
 @Service
@@ -13,16 +19,31 @@ public class BoardService {
     @Autowired//new를 써야하지만, 스프링부트가 알아서 읽어와서 주입을해준다.
     private BoardRepository boardRepository;
     //글작성 처리
-    public void write(Board board){
-    boardRepository.save(board);
+    public void write(Board board, MultipartFile file) throws IOException {
+        String projectPath=System.getProperty("user.dir")+
+                "\\src\\main\\resources\\static\\files";
+        UUID uuid=UUID.randomUUID();
+
+        String filename = uuid+"_"+file.getOriginalFilename();
+        File saveFile=new File(projectPath,filename);
+
+        file.transferTo(saveFile);
+
+        board.setFilename(filename);
+        board.setFilepath("\\src\\main\\resources\\static\\files"+filename);
+        boardRepository.save(board);
 
     }
-    //게시물 리스트 처리
-    public List<Board> boardList(){
-        //findAll : 테스트보드라는 클래스가 담긴 List를 반환하는것을 확인할수있다.
-        return boardRepository.findAll();
+    public Page<Board> boardSearchList(String searchKeyword, Pageable pageable){
+        return boardRepository.findByTitleContaining(searchKeyword,pageable);
     }
-    
+
+    //게시물 리스트 처리
+    public Page<Board> boardList(Pageable pageable){
+        //findAll : 테스트보드라는 클래스가 담긴 List를 반환하는것을 확인할수있다.
+        return boardRepository.findAll(pageable);
+    }
+
     //특정 게시물 불러오기
     public Board boardView(Integer id){
         System.out.println(boardRepository.findById(id).get());
